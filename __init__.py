404484ed711c3268e342d0c8978a04aecc93c668
jueming <jeming@cobra37.com>
2025-04-29 20:19:46 +0800

    delete edit node

--- __init__.py
+++ __init__.py
@@ -4,10 +4,6 @@ from .nodes.openai_image import CreateImageNode
 from .nodes.openai_save_image import OpenAISaveImageNode
 from .nodes.openai_save_to_file import OpenAI_SaveToFile
 
-# 导入自定义节点定义的 NODE_CLASS_MAPPINGS
-from .nodes.create_image_edit_node import NODE_CLASS_MAPPINGS as CREATE_IMAGE_EDIT_NODE_MAPPINGS
-from .nodes.create_image_edit_node import NODE_DISPLAY_NAME_MAPPINGS as CREATE_IMAGE_EDIT_DISPLAY_MAPPINGS
-
 NODE_CLASS_MAPPINGS = {
     "VideoCombineNode": VideoCombineNode,
     "AwsS3UploadNode": AwsS3UploadNode,
@@ -16,16 +12,10 @@ NODE_CLASS_MAPPINGS = {
     "OpenAI_SaveToFile": OpenAI_SaveToFile,
 }
 
-# 更新节点映射
-NODE_CLASS_MAPPINGS.update(CREATE_IMAGE_EDIT_NODE_MAPPINGS)
-
 NODE_DISPLAY_NAME_MAPPINGS = {
     "VideoCombineNode": "Video Combine",
     "AwsS3UploadNode": "AWS S3 Upload",
     "CreateImageNode": "Create Image (OpenAI)",
     "OpenAISaveImageNode": "Save Image (OpenAI)",
     "OpenAI_SaveToFile": "OpenAI SaveToFile",
-}
-
-# 更新显示名称映射
-NODE_DISPLAY_NAME_MAPPINGS.update(CREATE_IMAGE_EDIT_DISPLAY_MAPPINGS) 
+} 
--- web/index.js
+++ web/index.js
@@ -1,5 +1,4 @@
 // ComfyUI-ToolBox 前端入口文件
 // 导入所有扩展
 
-import "./js/index.js";
-import "./extensions/create_image_edit_frontend.js"; 
+import "./js/index.js"; 
